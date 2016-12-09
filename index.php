fce8ccd15a6ac26f917d2a2dccf016bd392e5555
Ryouga1009 <mznryg@gmail.com>
2016-12-09 17:46:18 +0900

    htdocs直下のindex

--- index.php
+++ index.php
@@ -1,37 +1,15 @@
 
 <?php
-
-//echo $_POST["post_1"];
-//$post = $_POST["post_1"];
-
-include 'config.php';
-
-  $linkDB = mysqli_connect("127.0.0.1", $dbuser, "test", "newdb");
-
+include 'xampp/config.php';
+  $linkDB = mysqli_connect("127.0.0.1", $dbuser,$dbpass,$dbname);
   if (!$linkDB) {
       echo "Error: Unable to connect to MySQL." . PHP_EOL;
       echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
       echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
       exit;
   }
-
-
-//mysqli_query($link,"INSERT INTO `table`(ID,T_Name,Flag) VALUES(1,'$_POST["post_1"]',1)");
- 
-//echo $_POST["post_1"];
-
-
-
-
-
-
-
 //データ内容更新
-if(mysqli_query($linkDB,"UPDATE `table` SET T_Name='$_POST[post_2]' WHERE ID=")){
+if(mysqli_query($linkDB,"UPDATE `table` SET Flag='$_POST[post_2]' WHERE T_Name='$_POST[post_1]'")){
   echo "Data updated<br \>";
 }else echo "not updated<br \>";
-
-
-
-
 ?>
